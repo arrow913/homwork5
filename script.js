6fe7c3eb1bda15e1585c976faf6e5286cb52b403
arrow913 <billy.arrasmith@gmail.com>
2020-10-28 18:15:34 -0500

    updating style.css color

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // listen for save button clicks
-    $(".saveBtn").on("click", function() {
-      // get nearby values
-      var value = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id");
-  
-      // save in localStorage
-      localStorage.setItem(time, value);
+    $(".saveBtn").on("click", function () {
+        // get nearby values
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // save in localStorage
+        localStorage.setItem(time, value);
     });
-  
+
     function hourUpdater() {
-      // get current number of hours
-      var currentHour = moment().hours();
-  
-      // loop over time blocks
-      $(".time-block").each(function() {
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
-  
-        // check if we've moved past this time
-        if (blockHour < currentHour) {
-          $(this).addClass("past");
-        } 
-        else if (blockHour === currentHour) {
-          $(this).removeClass("past");
-          $(this).addClass("present");
-        } 
-        else {
-          $(this).removeClass("past");
-          $(this).removeClass("present");
-          $(this).addClass("future");
-        }
-      });
+        // get current number of hours
+        var currentHour = moment().hours();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+            // check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
     }
-  
     hourUpdater();
-  
+
     // set up interval to check if current time needs to be updated
     var interval = setInterval(hourUpdater, 15000);
-  
+
     // load any saved data from localStorage
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
@@ -48,7 +47,7 @@ $(document).ready(function() {
     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  
+
     // display current day on page
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
-  });
+});
